82d132d945d925dd932f6c7ac15cd08db14b6df6
Benoît G <bgelineau.dev@gmail.com>
2021-02-15 23:26:05 +0100

    Organize commands

--- cmd/cli/quotes.go
+++ cmd/cli/quotes.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-    "errors"
-    "log"
-    "os"
+    "fmt"
 
     "github.com/leaanthony/clir"
 
@@ -13,26 +11,11 @@ import (
 func main() {
     cli := clir.NewCli("Quotes", "A basic wrapper to get financial asset quotes", "v0.0.1")
 
-    search := cli.NewSubCommand("search", "Search a financial asset")
-    search.LongDescription("Search a financial asset by name or ISIN and return the following information:\n\nSymbol | Name | Asset type | Last Price")
-    search.Action(func() error {
-        return handleAction(commands.Search)
-    })
+    commands.RegisterSearchAction(cli)
+    commands.RegisterGetAction(cli)
 
-    get := cli.NewSubCommand("get", "Return quotes")
-    get.Action(func() error {
-        return handleAction(commands.Get)
-    })
-
-    err := cli.Run()
-    if err != nil {
-        log.Fatal(err)
+    if err := cli.Run(); err != nil {
+        fmt.Printf("Error while trying to run the CLI:\n%v\n", err)
     }
 }
 
-func handleAction(executeAction commands.Command) error {
-    if len(os.Args) < 3 {
-        return errors.New("Missing a value, please refer to the documentation by using `quotes -help`")
-    }
-    return executeAction()
-}
--- internal/cli/commands.go
+++ internal/cli/commands.go
@@ -1,44 +1,58 @@
 package cli
 
 import (
+    "errors"
     "fmt"
     "os"
     "strings"
 
+    "github.com/leaanthony/clir"
     "github.com/olekukonko/tablewriter"
 
     "github.com/benoitgelineau/go-fetch-quotes/internal/utils"
 )
 
-type Command func() error
+func RegisterSearchAction(cli *clir.Cli) {
+    search := cli.NewSubCommand("search", "Search a financial asset")
+    search.LongDescription("Search a financial asset by name or ISIN and return the following information:\n\nSymbol | Name | Asset type | Last Price")
 
-func Search() error {
-    query := os.Args[2]
-
-    fmt.Printf("Searching for %s...\n", query)
-    assets := utils.ScrapeSearchResult(query)
-
-    if len(assets) == 0 {
-        fmt.Println("No result found.")
-    } else {
-        // Pretty print result in a table
-        table := tablewriter.NewWriter(os.Stdout)
-        table.SetHeader([]string{"Symbol", "Name", "Category", "Last price"})
-        table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
-        table.SetCenterSeparator("|")
-
-        lines := [][]string{}
-        for _, asset := range assets {
-            line := []string{asset.Symbol, asset.Name, asset.Category, asset.LastPrice}
-            lines = append(lines, line)
+    search.Action(func() error {
+        if len(os.Args) < 3 {
+            return errors.New("Missing a value, please refer to the documentation by using `quotes search -help`")
         }
 
-        table.AppendBulk(lines)
-        fmt.Println("Results found:\n")
-        table.Render()
-    }
+        // TODO handle string with space inside, encode URL
+        query := search.OtherArgs()[0]
 
-    return nil
+        fmt.Printf("Searching for %s...\n", query)
+        assets := utils.ScrapeSearchResult(query)
+
+        if len(assets) == 0 {
+            fmt.Println("No result found.")
+        } else {
+            // Pretty print result in a table
+            table := tablewriter.NewWriter(os.Stdout)
+            table.SetHeader([]string{"Symbol", "Name", "Category", "Last price"})
+            table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+            table.SetCenterSeparator("|")
+
+            lines := [][]string{}
+            for _, asset := range assets {
+                line := []string{asset.Symbol, asset.Name, asset.Category, asset.LastPrice}
+                lines = append(lines, line)
+            }
+
+            table.AppendBulk(lines)
+            fmt.Println("Results found:\n")
+            table.Render()
+        }
+
+        return nil
+    })
+}
+
+func RegisterGetAction(cli *clir.Cli) {
+    get := cli.NewSubCommand("get", "Return quotes")
 }
 
 func Get() error {
